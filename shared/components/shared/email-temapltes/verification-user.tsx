6b2d165a79c825da0a31fbb2de0478af714af067
Kami <120496647+Tina-TD@users.noreply.github.com>
2025-05-28 03:00:14 +0300

    Update verification-user.tsx

--- shared/components/shared/email-temapltes/verification-user.tsx
+++ shared/components/shared/email-temapltes/verification-user.tsx
@@ -10,7 +10,7 @@ export const VerificationUserTemplate = ({ code }: Props): JSX.Element => (
       Код подтверждения: <h2>{code}</h2>
     </p>
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
+      <a href={`https://oversize.vercel.app/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
     </p>
   </div>
 );
